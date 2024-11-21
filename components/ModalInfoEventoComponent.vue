c21c93aca3ba746913a9d4f2bdb2d185b639be59
Matias Roman <mroman.tdf@gmail.com>
2024-11-21 18:59:27 -0300

    Enhance ModalInfoEventoComponent: add SVG icon for directors, adjust styles for better visibility, and improve list item formatting

--- components/ModalInfoEventoComponent.vue
+++ components/ModalInfoEventoComponent.vue
@@ -11,6 +11,11 @@
             <ul>
                 <li v-for="integrante in props.evento.integrantes" :class="integrante.director ? 'director' : ''">
                     {{ integrante.nombre }}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" v-if="integrante.director">
+                        <title>compass-outline</title>
+                        <path
+                            d="M7,17L10.2,10.2L17,7L13.8,13.8L7,17M12,11.1A0.9,0.9 0 0,0 11.1,12A0.9,0.9 0 0,0 12,12.9A0.9,0.9 0 0,0 12.9,12A0.9,0.9 0 0,0 12,11.1M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" />
+                    </svg>
                 </li>
             </ul>
         </div>
@@ -107,14 +112,26 @@ const eliminarEvento = () => {
 
 .director {
     font-weight: bold;
+    font-size: 16px;
 }
 
 .director:hover {
     cursor: pointer;
+
 }
 
 .director:after {
-    content: '(Director)';
-    margin-left: 5px;
+    font-size: 20px;
+    margin-bottom: 5px;
+}
+
+svg {
+    width: 20px;
+    margin-bottom: 3px;
+}
+
+li {
+    list-style-type: none;
+    font-size: 16px;
 }
 </style>
