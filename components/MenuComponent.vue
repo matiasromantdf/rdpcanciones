ffccb123f544ba3556a5057675e4ad074c20f93e
Matias Roman <mroman.tdf@gmail.com>
2025-05-04 12:05:02 -0300

    Update navbar brand to display user email when logged in

--- components/MenuComponent.vue
+++ components/MenuComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top text-white">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">RDP</a>
+                <a class="navbar-brand" href="#">RDP - {{ usuario ? usuario.email : '' }}</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
