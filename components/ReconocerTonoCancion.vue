266d7c5a4fbbc499d0f10e497ce2ace909bb9709
Matias Roman <mroman.tdf@gmail.com>
2025-05-13 20:43:11 -0300

    Increase audio analysis timer to 60 seconds; update counter reset value for consistency.

--- components/ReconocerTonoCancion.vue
+++ components/ReconocerTonoCancion.vue
@@ -8,7 +8,7 @@
     const acumuladorChroma = new Array(12).fill(0);
     const emit = defineEmits(['nota-chroma', 'archivo-cargado']);
     const archivoCargado = ref(false);
-    const counter = ref(30);
+    const counter = ref(60);
 
     const majorProfile = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88];
     const minorProfile = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17];
@@ -84,7 +84,7 @@
             emit('nota-chroma', tonalidad);
             analyzing.value = false;
 
-        }, 30000); // 30 segundos
+        }, 60000); // 60 segundos
     }
 
     // contador para mostrar el tiempo restante
@@ -93,7 +93,7 @@
             if (analyzing.value) {
                 counter.value--;
                 if (counter.value <= 0) {
-                    counter.value = 30;
+                    counter.value = 60;
                 }
             }
         }, 1000);
